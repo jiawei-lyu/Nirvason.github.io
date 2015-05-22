ed389af3cfb3f8e3b2d4fe89cea31265b18dd11d
Nirvason <Nirvason@users.noreply.github.com>
2015-05-21 21:13:36 -0400

    Style Change, added palette.html/css

--- js/main.js
+++ js/main.js
@@ -1,32 +1,6 @@
 /**
  * Created by jiawei on 5/11/15.
  */
-//(function () {
-//    var one = document.getElementsByClassName('one')[0];
-//    document.addEventListener('click', function (e) {
-//        one.style.left = e.pageX;
-//        one.style.right = e.pageY;
-//        add('div');
-//    })
-//
-//    function add(type) {
-//        var element = document.createElement(type);
-//        //Assign different attributes to the element.
-//
-//        element.style.position = 'absolute';
-//        element.style.left = Math.random() * 100 + 'px';
-//        element.style.top = Math.random() * 100 + 'px';
-//        console.log(element.style.left );
-//        element.style.height = '100px';
-//        element.style.width = '100px';
-//        element.style.background = 'red';
-//
-//        var body = document.getElementsByTagName('body')[0];
-//        //Append the element in page (in span).
-//        body.appendChild(element);
-//    }
-//})();
-
 (function () {
     var links = document.getElementsByTagName('a');
     var len = links.length;
