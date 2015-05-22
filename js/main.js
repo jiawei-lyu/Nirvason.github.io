e71f7bdcbe7e2c41a71b642b391b8e47e5dc716a
Nirvason <Nirvason@users.noreply.github.com>
2015-05-22 14:49:07 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@
  * Created by jiawei on 5/11/15.
  */
 //(function () {
-//    var links = document.getElementsByTagName('a');
-//    var len = links.length;
-//    for (var i = 0; i < len; i++) {
-//        links[i].addEventListener('click', function (e) {
-//            console.log(this.style.display);
-//        });
-//    }
 //
 //    var cssPrefix = 'webkit';
 //    //var cssPrefix = false;
